8ef2b3e94dcbea199a2e4f487cc79dd4d9bfabf8
Catalin <catalin.caila@gmail.com>
2019-01-30 14:57:45 +0200

    add model classes

---
+++ ApplicationBank/src/main/java/com/userfront/demo/PrimaryTransaction.java
@@ -0,0 +1,92 @@
+package com.userfront.demo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class PrimaryTransaction {
+    private Long id;
+    private Date date;
+    private String description;
+    private String type;
+    private String status;
+    private double amount;
+    private BigDecimal availablBalance;
+    private PrimaryAccount primaryAccount;
+
+    public PrimaryTransaction(Date date, String description, String type, String status, double amount, BigDecimal availablBalance, PrimaryAccount primaryAccount) {
+        this.date = date;
+        this.description = description;
+        this.type = type;
+        this.status = status;
+        this.amount = amount;
+        this.availablBalance = availablBalance;
+        this.primaryAccount = primaryAccount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private PrimaryTransaction () {
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getAvailablBalance() {
+        return availablBalance;
+    }
+
+    public void setAvailablBalance(BigDecimal availablBalance) {
+        this.availablBalance = availablBalance;
+    }
+
+    public PrimaryAccount getPrimaryAccount() {
+        return primaryAccount;
+    }
+
+    public void setPrimaryAccount(PrimaryAccount primaryAccount) {
+        this.primaryAccount = primaryAccount;
+    }
+}
